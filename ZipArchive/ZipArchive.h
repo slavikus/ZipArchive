3a80b21538dd852472751f0b9641abb68ff35906
Slava Karpenko <slavikus@gmail.com>
2015-10-26 21:51:23 +0300

    Added xcodeproj with OS X target

---
+++ ZipArchive/ZipArchive.h
@@ -0,0 +1,10 @@
+//
+//  ZipArchive.h
+//  ZipArchive
+//
+//  Created by Slava Karpenko on 26.10.15.
+//  Copyright © 2015 Wildbit, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "SSZipArchive.h"
